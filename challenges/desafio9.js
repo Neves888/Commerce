f4490dceef5715a8a00364245082ef19c6f06faf
Neves888 <helen.vneves@gmail.com>
2023-01-15 22:32:46 -0300

    add req 10

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,1 +1,2 @@
-db.produtos.find({ "valoresNutricionais.0.quantidade": { $lt: 500 } }, { _id: 0, nome: 1 });
+db.produtos.find({ valoresNutricionais: { $elemMatch: 
+  { tipo: "calorias", quantidade: { $lt: 500 } } } }, { _id: 0, nome: 1 });
