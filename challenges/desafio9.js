877e59923e5455418192d82fbc473a7054409eaa
Neves888 <helen.vneves@gmail.com>
2023-01-15 22:26:42 -0300

    add req 9

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,2 +1,1 @@
-db.produtos.deleteMany({ curtidas: { $lt: 50 } });
-db.produtos.find({ }, { _id: 0, nome: 1 });
+db.produtos.find({ "valoresNutricionais.0.quantidade": { $lt: 500 } }, { _id: 0, nome: 1 });
